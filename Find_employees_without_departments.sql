ea74379020a3c178a5d150b761a7af5308070a12
Manikandan Gunasekaran <173633277+ManiTheAnalyst@users.noreply.github.com>
2026-03-18 12:41:31 +0530

    Update Find_employees_without_departments.sql

--- Find_employees_without_departments.sql
+++ Find_employees_without_departments.sql
@@ -27,4 +27,27 @@ INSERT INTO Employees (employee_id, employee_name, job_title, department_id) VAL
 (104, 'Ananya', 'Business Analyst', 99), 
 (105, 'Rahul', 'Operations Manager', 3);
 
+-- Option 1: Using Left Join and Null
 
+select e.employee_id,
+    e.employee_name,
+    e.job_title,
+    d.department_id
+from Employees e
+left join Departments d on e.department_id = d.department_id
+where d.department_id is NULL;
+
+
+---- Option 2: NOT EXISTS Subquery
+--Order of Execution from (Get all employees) ➡️ where (Filter them using the subquery) ➡️ select (Show only the requested columns for the remaining employees).
+
+select 
+    employee_id,
+    employee_name,
+    job_title
+from Employees e
+where not exists (
+    select 1 
+    from Departments d 
+    where d.department_id = e.department_id
+);
